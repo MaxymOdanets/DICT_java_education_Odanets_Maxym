cde5f4465fbea6f0a68718519c07203541fd2111
MaxymOdanets <m.r.odanets@student.khai.edu>
2023-12-05 10:18:53 +0200

    ChatBot 4-th stage

--- src/Chatbot/ChatBot.java
+++ src/Chatbot/ChatBot.java
@@ -2,13 +2,12 @@ package ChatBot;
 import java.util.Scanner;
 
 public class ChatBot {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! My name is " + "Aid" + ".");
         System.out.println("I was created in " + "2018" + ".");
         System.out.println("Please, remind me your name.");
-        System.out.println(">");
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
@@ -18,5 +17,10 @@ public class ChatBot {
         int r7 = scanner.nextInt();
         int age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int number = scanner.nextInt();
+        for (int i = 0; i <= number; i++) {
+            System.out.printf("%d\n", i);
+        }
     }
 }
